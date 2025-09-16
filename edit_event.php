33cb78eae67d9dc8439c6f3881799803d76544f0
Kunal Asude <143272678+KunalAsude@users.noreply.github.com>
2025-09-16 22:25:09 +0530

    comments

--- edit_event.php
+++ edit_event.php
@@ -7,7 +7,7 @@ if (isset($_POST['id'])) {
     $id = $_POST['id']; // Get the event ID
     $title = $_POST['title']; // Get the event title
     $start = $_POST['start']; // Get the event start date and time
-    $end = $_POST['end']; // Get the event end date and time
+    $end = $_POST['end']; // Get the event end date 
 
     // Prepare and execute the SQL statement to update the event
     $stmt = $conn->prepare("UPDATE events SET title = ?, start_datetime = ?, end_datetime = ? WHERE id = ?");

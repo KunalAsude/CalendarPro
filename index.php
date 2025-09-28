f5633a75316575f7d8fb42fe8fb92a5623c6a53e
Kunal Asude <143272678+KunalAsude@users.noreply.github.com>
2025-09-28 20:23:43 +0530

    Update modal title for event management

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ include 'includes/functions.php';
     <div id="calendar"></div>
 </div>
 
-<!-- Modal for adding/editing events -->
+<!-- Modal for adding or editing events -->
 <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="eventModalLabel">Add/Edit Event</h5>
+                <h5 class="modal-title" id="eventModalLabel">Add or Edit Event</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
